1aed20c93bd1bb713532c85bcf6fd1389d2bff15
fdelgado96 <fdelgado@itba.edu.ar>
2019-07-13 17:46:02 -0300

    Graphix

--- Analisis/fundamental.py
+++ Analisis/fundamental.py
@@ -4,19 +4,20 @@ import matplotlib.pyplot as plt
 
 highway_dfs = []
 densities = []
-car_amount = np.arange(50, 2000, 50)
-print(car_amount)
-highway_dfs.append(pd.read_csv('../data/length_50_lanes_5_cars_50_maxvelocity_10_simulation.csv'))
-highway_densities = [50/250]
-# convert from cars/cell^2 to cars/km^2
-highway_densities = [density*100 for density in highway_densities]
+cars_amounts = np.arange(50, 1050, 50)
+cars_amounts = np.append(cars_amounts, np.arange(1100, 3300, 200))
+for cars_amount in cars_amounts:
+    df = pd.read_csv(f'../data/cars_{cars_amount}_maxvel_6_brakeprob_0.2_lanechangeprob_0.5_simulation.csv')
+    highway_dfs.append(df)
+# convert from cars/cell to cars/m
+highway_densities = [cars_amount/(666*7.5) for cars_amount in cars_amounts]
 fluxes = []
-for i, highway_df in highway_dfs:
+for i, highway_df in enumerate(highway_dfs):
     highway_df = highway_df[highway_df['timestep'] >= 20]
-    mean_velocity = highway_df['velocity'].mean() * 7.5 * 3.6  # Convert from cells/second to km/hr
-    fluxes.append(mean_velocity*highway_densities[i])
+    mean_velocity = highway_df['velocity'].mean() * 7.5   # Convert from cells/second to m/s
+    fluxes.append(mean_velocity*(highway_densities[i]))
 
-plt.plot(highway_densities, fluxes)
-plt.ylabel(r'flujo del tráfico \mathrm{autos/hr}')
-plt.xlabel(r'densidad del tráfico en porcentaje de celdas ocupadas')
+plt.plot(highway_densities, fluxes, 'bo')
+plt.ylabel(r'flujo del tráfico autos/s')
+plt.xlabel(r'densidad del tráfico autos/m')
 plt.show()
